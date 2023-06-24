0451eb7e98b7dcd368127cb68536efd6e0681126
ReallyLastOne <reallylastone@op.pl>
2023-06-24 08:43:04 +0200

    Initial commit

---
+++ src/main/java/org/reallylastone/lichessbot/Main.java
@@ -0,0 +1,9 @@
+package org.reallylastone.lichessbot;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("Hello world!");
+	}
+}
